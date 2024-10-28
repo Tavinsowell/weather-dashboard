5a31382b5ef09ba8639d30f87bae103018e94f20
Tavin Sowell <tavinsowell@gmail.com>
2024-10-28 06:45:55 -0600

    render fix

--- Develop/client/vite.config.js
+++ Develop/client/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'nested/index.html'),
+      },
+    },
+  },
   server: {
     port: 3000,
     open: true,
