e0a36e2a61b4bc666911385eb8d1ea045e8c7eeb
Tavin Sowell <tavinsowell@gmail.com>
2024-10-28 07:06:35 -0600

    render fix

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -1,26 +1,19 @@
 import dotenv from 'dotenv';
 import express from 'express';
-
 dotenv.config();
 
 // Import the routes
 import routes from './routes/index.js';
 
-const PORT = process.env.PORT || 3001;
 const app = express();
 
+const PORT = process.env.PORT || 3001;
+
+// Serves static files in the entire client's dist folder
+app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/dist'));
-
-  app.get('*', (_req, res) => {
-    res.sendFile('../client/dist/index.html');
-  });
-}
-
-app.listen(PORT, () => {
-  console.log(`API server running on port ${PORT}!`);
-});
+// Start the server on the port
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
